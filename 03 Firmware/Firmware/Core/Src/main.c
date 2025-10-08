9e5bd1bb50cbaf074057eaa7350e4a3d46c1d220
erhan <erhan_cm@hotmail.com>
2025-10-08 19:51:46 +0200

    fix missing include

--- 03 Firmware/Firmware/Core/Src/main.c
+++ 03 Firmware/Firmware/Core/Src/main.c
@@ -24,7 +24,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "LED_Test.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
